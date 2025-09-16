9dc1c447f5dbb2f3768b7718e38a52f9e21253b3
YoungjinSeo <piggy0513@ajou.ac.kr>
2025-09-16 22:19:12 +0900

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        'react-router-dom', 
+        '@tanstack/react-query', 
+        'react-toastify',
+        '@emotion/react',
+        '@emotion/styled',
+        'react-icons/fa',
+        'react-spinners',
+        'react-hook-form',
+        'axios'
+      ]
+    }
+  }
 })
